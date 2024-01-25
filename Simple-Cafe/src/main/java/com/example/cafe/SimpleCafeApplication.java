990283bd67be633423cb9b5b38b08e5b6098e453
kimseok <ks31030>
2024-01-25 16:32:09 +0900

    ff

---
+++ Simple-Cafe/src/main/java/com/example/cafe/SimpleCafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCafeApplication.class, args);
+	}
+
+}
